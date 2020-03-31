39fcc5798a3e93e4ceed625e49557bd65bbd5eb9
Luis Müller <luis.mueller@campus.tu-berlin.de>
2020-03-31 13:45:09 +0200

    Added neccessary tests to Settings class

--- sleepy/gui/settings/v2/test/test_settings.py
+++ sleepy/gui/settings/v2/test/test_settings.py
@@ -7,12 +7,51 @@ from sleepy.gui.settings.v2.core import Settings
 
 class SettingsTest(unittest.TestCase):
 
-    def test_construct(self):
-        
+    def test_construct_attributes(self):
+
         settings = Settings()
 
         try:
             settings.showIndex
+            settings.intervalMin
+            settings.intervalMax
+            settings.useCheckpoints
+            settings.plotGrid
+            settings.plotGridSize
+            settings.plotFiltered
+
+            self.assertTrue(True)
+        except AttributeError:
+            self.assertFalse(True)
+
+    def test_construct_useCheckpoints(self):
+
+        settings = Settings()
+
+        try:
+
+
+            self.assertTrue(True)
+        except AttributeError:
+            self.assertFalse(True)
+
+    def test_construct_intervalMinMax(self):
+
+        settings = Settings()
+
+        try:
+            settings.intervalMin
+            settings.intervalMax
+
+            self.assertTrue(True)
+        except AttributeError:
+            self.assertFalse(True)
+
+    def test_construct_intervalMinMax(self):
+
+        settings = Settings()
+
+        try:
 
             self.assertTrue(True)
         except AttributeError:
