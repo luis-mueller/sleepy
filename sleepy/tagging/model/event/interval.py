08f367001e7257947aa2f42e179de9768a2922f9
Luis Müller <luis.mueller@campus.tu-berlin.de>
2020-04-01 12:50:53 +0200

    hotfix polygon has no property c

--- sleepy/tagging/model/event/interval.py
+++ sleepy/tagging/model/event/interval.py
@@ -49,7 +49,7 @@ class IntervalEvent(Event):
 
     def plotSelected(self, axis):
 
-        axis.axvspan(*self.intervalInSeconds, alpha = .5, c=self.applicationSettings.plotSelectedColor)
+        axis.axvspan(*self.intervalInSeconds, alpha = .5, color=self.applicationSettings.plotSelectedColor)
 
     def plotVisible(self, axis):
 
