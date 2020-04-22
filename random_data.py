e7dfcb9a11b0a80b1440c88416fd0c295bd98c50
Luis Müller <luis.mueller@campus.tu-berlin.de>
2020-04-22 22:51:44 +0200

    Adjusted the show cases of the test signals

--- random_data.py
+++ random_data.py
@@ -1,12 +1,6 @@
 
 from sleepy.test.data import TestSignal
-from sleepy.processing.algorithms.massimi import Massimi
-from sleepy.processing.filters.bandpass.core import BandPassFilter
 import matplotlib.pyplot as plt
-
-# Prepare the algorithm and filter instances (we keep the standard values)
-algorithm = Massimi().render()
-filter = BandPassFilter().render()
 
 # Create the x- and y-axis data of a signal with std. dev. 2, 300 samples per sub-wave and 50 sub-waves.
 x, y = TestSignal.generate(scale = 2, numberOfSamples = 300, size=50)
